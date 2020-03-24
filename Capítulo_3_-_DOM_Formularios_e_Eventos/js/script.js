e1e9f5764e140c226842dbe3cdfc3989ce39189e
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-03-24 14:49:37 -0300

    Commit inicial do capítulo 3

---
+++ Capítulo_3_-_DOM_Formularios_e_Eventos/js/script.js
@@ -0,0 +1,26 @@
+console.log('Olá mundo!');
+
+var h1 = document.querySelector('h1');
+var classeDadosPessoais = document.querySelector('.dados-pessoais');
+var img = document.querySelector('#rosto');
+
+console.log(`Conteúdo do H1: ${h1.textContent}`);
+console.log(
+  `Conteúdo do elemento com classe dados-pessoais: ${classeDadosPessoais.textContent}`
+);
+console.log(`Conteúdo do elemento com id 'rosto' ${img}`);
+
+var th = document.querySelectorAll('th');
+console.log(th);
+
+console.log(
+  `Convertendo NodeList em array: ${Array.from(th).map(
+    item => item.textContent
+  )}`
+);
+
+var data = Array.from(document.querySelectorAll('.data'));
+for (let i = 0; i < data.length; i++) {
+  let currentElement = data[i];
+  currentElement.classList.add('emphasis');
+}
